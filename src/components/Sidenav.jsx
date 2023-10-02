a5556e9deebc4ef991846d6562bf1d2d64024775
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2023-10-02 22:15:07 +0545

    Few changes

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,9 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
+import {RxDashboard} from 'react-icons/rx'
 
 const Sidenav = () => {
-  return (
-    <Box as="aside">Sidenav</Box>
+  return (<>
+  <Box as="aside">Sidenav</Box>
+  </>
+    
+   
   )
 }
 
