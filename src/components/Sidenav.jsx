118260fc976239ef81314421eb57c8092caf553b
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2023-10-04 02:09:43 +0545

    stack and hstack

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,10 +1,31 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Stack } from '@chakra-ui/react'
 import React from 'react'
 import {RxDashboard} from 'react-icons/rx'
 
 const Sidenav = () => {
+
+  const navLinks = [
+    {
+      icon: RxDashboard,
+      text: "Dashboard",
+      link: "/",
+    },
+    {
+      icon: RxDashboard,
+      text: "Dashboard",
+      link: "/",
+    },
+  ];
+
   return (<>
-  <Box as="aside">Sidenav</Box>
+  <Stack>
+    {
+      navLinks.map(nav=>
+        <HStack></HStack>
+        )
+    }
+
+  </Stack>
   </>
     
    
