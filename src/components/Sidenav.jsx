6791f94907d8e388b2c743e433b917540fdb7cc8
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2023-10-07 22:55:09 +0545

    SIDEBAR

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Icon, Stack, Text } from '@chakra-ui/react'
+import { Box, HStack, Heading, Icon, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import {RxDashboard} from 'react-icons/rx'
 import {BsArrowDownUp} from 'react-icons/bs'
@@ -18,8 +18,10 @@ const Sidenav = () => {
     },
   ];
 
-  return (<>
-  <Stack>
+  return (
+  <>
+  <Stack boxShadow="sm" max-width="256px" height="100vh">
+    <Heading as="h1">@BIVEKCRYPTO</Heading>
     {
       navLinks.map((nav)=>
         <HStack key={nav.text}>
