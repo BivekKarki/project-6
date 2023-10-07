74b794dc5b4378c75cd47cb35f12ffd88e7ef5b2
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2023-10-07 22:50:32 +0545

    icon and text from chakra ui

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,7 @@
-import { Box, HStack, Stack } from '@chakra-ui/react'
+import { Box, HStack, Icon, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import {RxDashboard} from 'react-icons/rx'
+import {BsArrowDownUp} from 'react-icons/bs'
 
 const Sidenav = () => {
 
@@ -11,17 +12,20 @@ const Sidenav = () => {
       link: "/",
     },
     {
-      icon: RxDashboard,
-      text: "Dashboard",
-      link: "/",
+      icon: BsArrowDownUp,
+      text: "Transaction",
+      link: "/transactions",
     },
   ];
 
   return (<>
   <Stack>
     {
-      navLinks.map(nav=>
-        <HStack></HStack>
+      navLinks.map((nav)=>
+        <HStack key={nav.text}>
+          <Icon as={nav.icon}/>
+          <Text>{nav.text}</Text>
+        </HStack>
         )
     }
 
